a0ad2317ce69e0524bb347a4487c52505215ab9f
t_kasajima <hi_tech.iyosi@shigokiru.com>
2019-03-28 17:11:02 +0900

    Update perfmon.ps1
    DISK関連をシンプルにした

--- perfmon.ps1
+++ perfmon.ps1
@@ -1,51 +1,30 @@
 $filename = Get-Date -Format "yyyy-MMdd"
-$beforetotalR = 0
-$beforetotalW = 0
 
 while(1){
     
     #CPUのProcessorTime取得
     $dataproc = Get-Counter -Counter "\Processor Information(_Total)\% Processor Time"
-    $datatest1 = Get-Counter -Counter "\PhysicalDisk(_Total)\% Idle Time"
-    $datatest1 = Get-Counter -Counter "\PhysicalDisk(_Total)\% Disk Time"
+    $item = $dataproc.CounterSamples.CookedValue
+
+    #Diskデータ取得
+    $diskidle = Get-Counter -Counter "\PhysicalDisk(_Total)\% Idle Time"
+    $disktime = Get-Counter -Counter "\PhysicalDisk(_Total)\% Disk Time"
+    $dateidle = $diskidle.CounterSamples.CookedValue
+    $datedisk = $disktime.CounterSamples.CookedValue
 
     #メモリデータ取得
     $datamem  = Get-WmiObject Win32_OperatingSystem
     $diskinfo = Get-WmiObject Win32_PerfRawData_PerfDisk_PhysicalDisk
-    $datedisk = $diskinfo | ?{$_.Name -eq "_Total"}
     
     #タイムスタンプ取得
     $datetime = $dataproc.Timestamp.ToString('yyyy/mm/dd HH:mm:ss')
-    $i = 1
-    foreach($item in $dataproc.CounterSamples) {
-        if($i -eq 1){
-            if($beforetotalR -eq 0 -And $beforetotalW -eq 0){
-                $beforetotalR = $datedisk.PercentDiskReadTime
-                $beforetotalW = $datedisk.PercentDiskWriteTime
-                $aftertotalR = $beforetotalR
-                $aftertotalW = $beforetotalW
-            }else{
-                $beforetotalR = $datedisk.PercentDiskReadTime
-                $beforetotalW = $datedisk.PercentDiskWriteTime
-                if($aftertotalR -ne $beforetotalR){
-                    $execR = $beforetotalR - $aftertotalR
-                    $aftertotalR = $beforetotalR
-                }
-                if($aftertotalW -ne $beforetotalW){
-                    $execW = $beforetotalW - $aftertotalW
-                    $aftertotalW = $beforetotalW
-                }
-            }
-            Write-Output(@($item.CookedValue,  #CPUのトータルのみ出力`
-                            $datamem.FreePhysicalMemory, $datamem.TotalVisibleMemorySize, #物理メモリの空き容量と全体容量`
-                            $datamem.FreeVirtualMemory, $datamem.TotalVirtualMemorySize, #仮想メモリの空き容量と全体容量`
-                            $execR, $execW,  #DiskのReadとWrite`
-                            $datetime) -join "`t") | out-file -Append "perflog_$filename.csv"
-            $i = $i + 1
-        }else{
-            break
-        }
-    }
+
+    #アウトプット
+    Write-Output(@($item,  #CPUのトータルのみ出力`
+                   $datamem.FreePhysicalMemory, $datamem.TotalVisibleMemorySize, #物理メモリの空き容量と全体容量`
+                   $datamem.FreeVirtualMemory, $datamem.TotalVirtualMemorySize, #仮想メモリの空き容量と全体容量`
+                   $datedisk, $dateidle, #DiskTimeとIdleTime`
+                   $datetime) -join "`t") | out-file -Append "perflog_$filename.csv"   
     #30秒待機
     Start-Sleep -Seconds 1
 }
